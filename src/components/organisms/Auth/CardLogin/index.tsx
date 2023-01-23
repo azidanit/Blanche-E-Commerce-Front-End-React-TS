7757013e185e52fd5cd7fa257fa609a53d68eabb
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2023-01-23 17:21:58 +0700

    feat: card form login

--- src/components/organisms/Auth/CardLogin/index.tsx
+++ src/components/organisms/Auth/CardLogin/index.tsx
@@ -1,13 +1,35 @@
+import { Form } from 'antd';
 import React from 'react';
-import { Card } from '../../../atoms';
+import { Button, Card, FormLabel, Input, InputPassword } from '../../../atoms';
 import style from './index.module.scss';
+import useForm from './useForm';
+import { rules } from './rules';
 
 const CardLogin = (): JSX.Element => {
+  const { handleSubmit } = useForm();
+
   return (
     <Card className={style.card__login}>
       <>
         <h6>Hey Hello👋</h6>
         <p>Lorem ipsum dolor sit amet.</p>
+
+        <Form
+          name="basic"
+          layout="vertical"
+          initialValues={{ remember: true }}
+          onFinish={handleSubmit}
+          autoComplete="off"
+        >
+          <FormLabel label="Email" name="email" rules={rules.email}>
+            <Input />
+          </FormLabel>
+
+          <FormLabel label="Password" name="password" rules={rules.password}>
+            <InputPassword />
+          </FormLabel>
+          <Button type="primary">Submit</Button>
+        </Form>
       </>
     </Card>
   );
