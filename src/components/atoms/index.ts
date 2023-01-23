5be62c4cf6e488e792e0bc186f4330ac48d1913e
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2023-01-23 17:22:40 +0700

    feat: import input compo  to root index

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -3,5 +3,7 @@ import Button from './Button';
 import Image from './Image';
 import Input from './Input';
 import Card from './Card';
+import FormLabel from './FormLabel';
+import InputPassword from './InputPassword';
 
-export { Logo, Button, Image, Input, Card };
+export { Logo, Button, Image, Input, Card, FormLabel, InputPassword };
