acb04ce73761eacd107612d401a751685228a3e8
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2023-01-24 18:51:22 +0700

    feat: create type entity for product detail and its dependency

--- src/helpers/types/entity.interface.ts
+++ src/helpers/types/entity.interface.ts
@@ -16,6 +16,51 @@ export interface IUser {
   role?: string;
 }
 
+export interface IMerchant {
+  name: string;
+  domain: string;
+  image_url: string;
+  avg_rating: number;
+}
+
+export interface ICategory {
+  name: string;
+  url: string;
+}
+
+export interface IImage {
+  image_url: string;
+}
+
+export interface IRating {
+  avg_rating: number;
+  count: number;
+}
+
+export interface IDimension {
+  width: number;
+  length: number;
+  height: number;
+}
+
+export interface IVariantOption {
+  name: string;
+  value: string[];
+}
+
+export interface IVarianItem {
+  id: number;
+  is_archived: boolean;
+  image: string;
+  price: number;
+  stock: number;
+}
+
+export interface IVariant {
+  variant_option: IVariantOption[];
+  variant_items: IVarianItem[];
+}
+
 export interface IProduct {
   id: number;
   title: string;
@@ -28,4 +73,27 @@ export interface IProduct {
   thumbnail_img: string;
   slug: string;
   seller_city: string;
+}
+
+export interface IProductDetail {
+  id: number;
+  merchant_id: number;
+  category_id: number;
+  category: ICategory;
+  title: string;
+  description: string;
+  min_real_price: number;
+  max_real_price: number;
+  min_discount_price: number;
+  max_discount_price: number;
+  unit_sold: number;
+  images: IImage[];
+  weight: number;
+  dimension: IDimension;
+  SKU: string;
+  favorite_count: number;
+  total_stock: number;
+  status: string;
+  rating: IRating;
+  merchant: IMerchant;
 }
