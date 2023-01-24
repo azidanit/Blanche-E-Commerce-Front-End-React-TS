3530c697a5d4adb22ce7c105003b6bdb8bad499e
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2023-01-24 11:58:29 +0700

    feat: customize theme

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -1,6 +1,10 @@
 const theme = {
   token: {
     colorPrimary: '#00adef',
+    borderRadius: 5,
+    colorError: 'rgb(239, 20, 74)',
+    fontFamily: 'Poppins, sans-serif',
+    colorTextBase:' '
   },
 };
 
