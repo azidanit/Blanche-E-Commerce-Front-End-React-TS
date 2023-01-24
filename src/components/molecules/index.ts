7c8b30b6802071ff563a483d31213be737600468
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2023-01-24 18:53:32 +0700

    feat: add styling to cart in home

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,6 +1,15 @@
 import AuthSectionLeft from './AuthSectionLeft';
 import AuthSectionRight from './AuthSectionRight';
+import InputQuantity from './InputQuantity';
 import Container from './Container';
 import Form from './Form';
+import CartItem from './CartButton/CartItem';
 
-export { AuthSectionLeft, AuthSectionRight, Container, Form };
+export {
+  AuthSectionLeft,
+  AuthSectionRight,
+  Container,
+  Form,
+  InputQuantity,
+  CartItem,
+};
